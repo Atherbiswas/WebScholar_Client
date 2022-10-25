efaf066febbf65f7048ff7f1feda86c08ee6c53a
Ather1230 <ather.biswas0057@gmail.com>
2022-10-25 23:04:04 +0600

    added our team section

--- src/pages/Other/About/About.js
+++ src/pages/Other/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaBookOpen, FaUserTie, FaUserFriends } from 'react-icons/fa';
+import './About.css'
 
 const About = () => {
     return (
@@ -7,7 +8,7 @@ const About = () => {
             <h1 className='text-center mb-3'>Our Manngement</h1>
             <div className='row'>
                 <div className='col-lg-4 col-md-4 col-12'>
-                    <div className='text-center bg-info p-1 rounded-4'>
+                    <div className='text-center bg-about p-1 rounded-4'>
                     <div><FaBookOpen className='fs-1'></FaBookOpen></div>
                     <hr />
                     <h1>10+</h1>
@@ -15,7 +16,7 @@ const About = () => {
                     </div>
                 </div>
                 <div className='col-lg-4 col-md-4 col-12'>
-                <div className='text-center bg-info p-1 rounded-4'>
+                <div className='text-center bg-about p-1 rounded-4'>
                     <div><FaUserTie className='fs-1'></FaUserTie></div>
                     <hr />
                     <h1>25+</h1>
@@ -23,7 +24,7 @@ const About = () => {
                     </div>
                 </div>
                 <div className='col-lg-4 col-md-4 col-12'>
-                <div className='text-center bg-info p-1 rounded-4'>
+                <div className='text-center bg-about p-1 rounded-4'>
                     <div><FaUserFriends className='fs-1'></FaUserFriends></div>
                     <hr />
                     <h1>2.5k +</h1>
--- src/pages/Other/OurTeam/OurTeam.js
+++ src/pages/Other/OurTeam/OurTeam.js
@@ -1,9 +1,145 @@
 import React from 'react';
+import './OurTeam.css';
+import { FaFacebookF, FaTwitter, FaLinkedinIn, FaInstagram, FaArrowCircleRight } from 'react-icons/fa';
+import Button from 'react-bootstrap/Button';
+import team1 from '../../../team-img/team-1.jpg';
+import team2 from '../../../team-img/team-2.jpg';
+import team3 from '../../../team-img/team-3.jpg';
+import team4 from '../../../team-img/team-4.jpg';
+import team5 from '../../../team-img/team-5.jpg';
+import team6 from '../../../team-img/team-6.jpg';
+import team7 from '../../../team-img/team-7.jpg';
+import team8 from '../../../team-img/team-9.jpg'
 
 const OurTeam = () => {
     return (
-        <div>
-            <h1>This our team section</h1>
+        <div className='container section-padding'>
+            <h1 className='text-center'>Our Educator</h1>
+            <div className='row g-3 mt-2'>
+                <div className='col-12 col-md-6 col-lg-3'>
+                    <div className='card text-center'>
+                        <div className='card-body shadow'>
+                            <img src={team1} className='img-fluid rounded-circle' alt="" />
+                            <h4 className='card-title'>Ayat Nurjahan</h4>
+                            <p className='card-text'>Educator</p>
+                            <p className='socials'>
+                                <FaFacebookF className='mx-1 fs-4'></FaFacebookF>
+                                <FaTwitter className='mx-1 fs-4'></FaTwitter>
+                                <FaLinkedinIn className='mx-1 fs-4'></FaLinkedinIn>
+                                <FaInstagram className='mx-1 fs-4'></FaInstagram>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-3'>
+                    <div className='card text-center'>
+                        <div className='card-body shadow'>
+                            <img src={team2} className='img-fluid rounded-circle' alt="" />
+                            <h4 className='card-title'>Abdus Arif</h4>
+                            <p className='card-text'>Educator</p>
+                            <p className='socials'>
+                                <FaFacebookF className='mx-1 fs-4'></FaFacebookF>
+                                <FaTwitter className='mx-1 fs-4'></FaTwitter>
+                                <FaLinkedinIn className='mx-1 fs-4'></FaLinkedinIn>
+                                <FaInstagram className='mx-1 fs-4'></FaInstagram>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-3'>
+                    <div className='card text-center'>
+                        <div className='card-body shadow'>
+                            <img src={team3} className='img-fluid rounded-circle' alt="" />
+                            <h4 className='card-title'>Abdul Arif</h4>
+                            <p className='card-text'>Educator</p>
+                            <p className='socials'>
+                                <FaFacebookF className='mx-1 fs-4'></FaFacebookF>
+                                <FaTwitter className='mx-1 fs-4'></FaTwitter>
+                                <FaLinkedinIn className='mx-1 fs-4'></FaLinkedinIn>
+                                <FaInstagram className='mx-1 fs-4'></FaInstagram>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-3'>
+                    <div className='card text-center'>
+                        <div className='card-body shadow'>
+                            <img src={team8} className='img-fluid rounded-circle' alt="" />
+                            <h4 className='card-title'>Nipah Mahmuda</h4>
+                            <p className='card-text'>Educator</p>
+                            <p className='socials'>
+                                <FaFacebookF className='mx-1 fs-4'></FaFacebookF>
+                                <FaTwitter className='mx-1 fs-4'></FaTwitter>
+                                <FaLinkedinIn className='mx-1 fs-4'></FaLinkedinIn>
+                                <FaInstagram className='mx-1 fs-4'></FaInstagram>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-3'>
+                    <div className='card text-center'>
+                        <div className='card-body shadow'>
+                            <img src={team5} className='img-fluid rounded-circle' alt="" />
+                            <h4 className='card-title'>Rahman Ujjal</h4>
+                            <p className='card-text'>Educator</p>
+                            <p className='socials'>
+                                <FaFacebookF className='mx-1 fs-4'></FaFacebookF>
+                                <FaTwitter className='mx-1 fs-4'></FaTwitter>
+                                <FaLinkedinIn className='mx-1 fs-4'></FaLinkedinIn>
+                                <FaInstagram className='mx-1 fs-4'></FaInstagram>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-3'>
+                    <div className='card text-center'>
+                        <div className='card-body shadow'>
+                            <img src={team6} className='img-fluid rounded-circle' alt="" />
+                            <h4 className='card-title'>Raihan Sabbir</h4>
+                            <p className='card-text'>Educator</p>
+                            <p className='socials'>
+                                <FaFacebookF className='mx-1 fs-4'></FaFacebookF>
+                                <FaTwitter className='mx-1 fs-4'></FaTwitter>
+                                <FaLinkedinIn className='mx-1 fs-4'></FaLinkedinIn>
+                                <FaInstagram className='mx-1 fs-4'></FaInstagram>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-3'>
+                    <div className='card text-center'>
+                        <div className='card-body shadow'>
+                            <img src={team7} className='img-fluid rounded-circle' alt="" />
+                            <h4 className='card-title'>Mahmud Mohammad</h4>
+                            <p className='card-text'>Educator</p>
+                            <p className='socials'>
+                                <FaFacebookF className='mx-1 fs-4'></FaFacebookF>
+                                <FaTwitter className='mx-1 fs-4'></FaTwitter>
+                                <FaLinkedinIn className='mx-1 fs-4'></FaLinkedinIn>
+                                <FaInstagram className='mx-1 fs-4'></FaInstagram>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className='col-12 col-md-6 col-lg-3'>
+                <div className='card text-center'>
+                        <div className='card-body shadow'>
+                            <img src={team4} className='img-fluid rounded-circle' alt="" />
+                            <h4 className='card-title'>Royal Malek</h4>
+                            <p className='card-text'>Educator</p>
+                            <p className='socials'>
+                                <FaFacebookF className='mx-1 fs-4'></FaFacebookF>
+                                <FaTwitter className='mx-1 fs-4'></FaTwitter>
+                                <FaLinkedinIn className='mx-1 fs-4'></FaLinkedinIn>
+                                <FaInstagram className='mx-1 fs-4'></FaInstagram>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <p className='text-center mt-3'>
+            <Button variant="warning"  size="lg">SEE ALL <FaArrowCircleRight></FaArrowCircleRight> </Button>
+            </p>
         </div>
     );
 };
